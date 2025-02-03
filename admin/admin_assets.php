c2f4937df4cd8ed81b7fb529c375042a157f4d23
Zearoarc <matthewdiola7@gmail.com>
2025-02-03 23:06:05 +0800

    assets and manage fix

--- admin/admin_assets.php
+++ admin/admin_assets.php
@@ -34,42 +34,6 @@ $result = $conn->query($sql);
     </head>
 
     <body>
-<<<<<<< HEAD
-        <div class="container-fluid">
-
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-
-                    <body>
-                        <div class="container-fluid">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <a class="btn btn-primary" href="admin_assets_add.php" role="button">Add</a>
-                                </div>
-
-                                <table class="table table-bordered">
-                                    <thead class="table-blue">
-                                        <tr>
-                                            <th scope="col">ID</th>
-                                            <th scope="col">Type</th>
-                                            <th scope="col">Supplier</th>
-                                            <th scope="col">Asset Model</th>
-                                            <th scope="col">Department</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        if ($result->num_rows > 0) {
-                                            while ($row = $result->fetch_assoc()) {
-                                                $supplierLogo = "/images/{$row['supplier']}.jpg";
-                                                $status = "Available";
-                                                if ($status == null) {
-                                                    $status = "Unavailable";
-                                                }
-                                                echo "<tr>
-=======
 
         <body>
             <div class="container-fluid">
@@ -100,7 +64,6 @@ $result = $conn->query($sql);
                                         $status = "Unavailable";
                                     }
                                     echo "<tr>
->>>>>>> a838262168d5fc9cb994ed4df2f448f01fd29e12
                                 <td>{$row['id']}</td>
                                 <td>{$row['type']}</td>
                                 <td>{$row['supplier']}</td>
@@ -112,19 +75,6 @@ $result = $conn->query($sql);
     <button class ='btn btn-danger'><a href='delete.php?deleteid='..'' class='text-light'>Cancel</a></button>
   </td>
                               </tr>";
-<<<<<<< HEAD
-                                            }
-                                        } else {
-                                            echo "<tr><td colspan='4' class='text-center'>No records found</td></tr>";
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-                    </body>
-=======
                                 }
                             } else {
                                 echo "<tr><td colspan='4' class='text-center'>No records found</td></tr>";
@@ -136,7 +86,6 @@ $result = $conn->query($sql);
                 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         </body>
->>>>>>> a838262168d5fc9cb994ed4df2f448f01fd29e12
 
     </html>
 
